99f83870c28cd67479b32a951e24f079af2aa213
Donghwan Lee <dhwanyi@gmail.com>
2026-05-09 13:52:30 -0700

    Add lower() for case-insensitivity

--- Day_3/Treasure_Island.py
+++ Day_3/Treasure_Island.py
@@ -24,15 +24,15 @@ print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure")
 print("You're at a cross road. Where do you want to go?")
 
-direction = input("      Type \"left\" or \"right\"\n")
+direction = input("      Type \"left\" or \"right\"\n").lower()
 if direction == "left":
     print("You've come to a lake. There is an island in the middle of the lake.")
-    swim_or_wait = input("      Type \"wait\" to wait for a boat. Type \"swim\" to swim across.\n")
+    swim_or_wait = input("      Type \"wait\" to wait for a boat. Type \"swim\" to swim across.\n").lower()
     if swim_or_wait == "swim":
         print("You get attacked by an angry trout. Game Over.")
     else:
         print("You arrive at the island unharmed. There is a house with 3 doors.")
-        red_yellow_blue = input("      One red, one yellow, and one blue. Which colour do you choose?\n")
+        red_yellow_blue = input("      One red, one yellow, and one blue. Which colour do you choose?\n").lower()
         
         if red_yellow_blue == "red":
             print("It's a room full of fire. Game Over.")
