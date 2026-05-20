c57ecad3c660f4a3daed0858a3fbe7da6e45c78d
Donghwan Lee <dhwanyi@gmail.com>
2026-05-20 16:37:30 -0700

    Day 18 Project: Turtle Challenge 5 - Draw a Spirograph

--- Day_18/turtle_challenge.py
+++ Day_18/turtle_challenge.py
@@ -43,14 +43,31 @@ colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSea
 #     draw_shape(shape_side_n)
 
 ########### Challenge 4 - Random Walk ########
+t.colormode(255)
 # directions = [0, 90, 180, 270]
 # tim.pensize(15)
-# tim.speed("fastest")
+tim.speed("fastest")
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
 
 # for _ in range(100):
-#     tim.color(random.choice(colours))
+#     tim.color(random_color())
 #     tim.forward(30)
 #     tim.setheading(random.choice(directions))
 
-# screen = t.Screen()
-# screen.exitonclick()
+########### Challenge 5 - Spirograph ########
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap) ):
+        tim.color(random_color())
+        tim.circle(100)
+        current_heading = tim.heading()
+        tim.setheading(current_heading + size_of_gap)
+
+draw_spirograph(5)
+
+screen = t.Screen()
+screen.exitonclick()
